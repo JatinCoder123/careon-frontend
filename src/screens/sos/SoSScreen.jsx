8253129909ddb60151e4690d8c523ef585dd0097
Jatin Verma <verm.jatin2004@gmail.com>
2026-01-30 00:22:41 +0530

    Working on app

---
+++ src/screens/sos/SoSScreen.jsx
@@ -0,0 +1,8 @@
+import { View, Text } from "react-native";
+export default function SoSScreen() {
+  return (
+    <View >
+      <Text>SOS Screen</Text>
+    </View>
+  );
+}
--- src/services/googleAuth.service.js
+++ src/services/googleAuth.service.js
@@ -1,15 +1,23 @@
 import * as WebBrowser from "expo-web-browser";
 import * as Google from "expo-auth-session/providers/google";
-
+import * as AuthSession from "expo-auth-session";
 WebBrowser.maybeCompleteAuthSession();
-
+const redirectUri = AuthSession.makeRedirectUri({
+  scheme: "careon",
+  path: "redirect",
+});
 export const useGoogleAuth = () => {
-    const [request, response, promptAsync] = Google.useAuthRequest({
-        androidClientId: "77275497226-3jukkh5dugm520qbotpm641a8ipiar26.apps.googleusercontent.com",
-        iosClientId: "77275497226-29sdprktl1gapn0loec4otpli28fkhcc.apps.googleusercontent.com",
-        webClientId: "77275497226-6f7rm1i1c9v02eupv2gttgvpe640f1gq.apps.googleusercontent.com",
-        scopes: ["profile", "email"],
-    });
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    androidClientId:
+      "77275497226-3jukkh5dugm520qbotpm641a8ipiar26.apps.googleusercontent.com",
+    iosClientId:
+      "77275497226-29sdprktl1gapn0loec4otpli28fkhcc.apps.googleusercontent.com",
+    webClientId:
+      "77275497226-k3udb03liq57gur0207dhhbsn0nf3q0g.apps.googleusercontent.com",
+    scopes: ["profile", "email"],
+    // redirectUri,
+  });
 
-    return { request, response, promptAsync };
+  return { request, response, promptAsync };
 };
+//client secret : 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
